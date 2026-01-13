dba628a15f0583d787fd57348f594477a43f7e8c
Lucky-259 <1436596125@qq.com>
2026-01-13 21:00:56 +0800

    update

--- scripts/rl_1.5b_baseline_if.sh
+++ scripts/rl_1.5b_baseline_if.sh
@@ -47,10 +47,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.max_num_batched_tokens=32768 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
-    actor_rollout_ref.rollout.temperature=0.6 \
+    actor_rollout_ref.rollout.temperature=0.9 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
+    actor_rollout_ref.rollout.val_kwargs.temperature=0.9 \
     actor_rollout_ref.rollout.val_kwargs.n=8 \
     trainer.critic_warmup=0 \
     trainer.logger='["console"]' \
