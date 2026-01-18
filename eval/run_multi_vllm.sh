9e7ec7afc3fd6a5bd3fde3430d5b5d32364fec3b
Lucky-259 <1436596125@qq.com>
2026-01-18 20:54:19 +0800

    update

--- eval/run_multi_vllm.sh
+++ eval/run_multi_vllm.sh
@@ -12,9 +12,12 @@ BASE_SCRIPT="eval/vllm_all_8k.sh"   # 改成你的实际脚本路径
 
 # 你可以在这里列出多组：PROJECT_NAME EXPERIMENT_NAME [REPEAT] [CONCURRENCY]
 JOBS=(
-  "Redundancy DS1.5B_8k_redundancy_ratio_2_self 16 200"
-  "ShorterBetter DS1.5B_8k_shorter_1e-3 16 200"
-  "ShorterBetter DS1.5B_16k_shorter_1e-4 16 200"
+  "Redundancy DS1.5B_8k_redundancy_correct_self_1_1e-4 1 200"
+  "Redundancy DS1.5B_8k_redundancy_correct_self_1_2e-4 1 200"
+  "Redundancy DS1.5B_8k_redundancy_full_ground_1_2e-4 1 200"
+  "Redundancy DS1.5B_8k_redundancy_ratio_1_self 1 200"
+  "ShorterBetter DS1.5B_8k_shorter_1e-3 1 200"
+  "ShorterBetter DS1.5B_16k_shorter_1e-4 1 200"
 )
 
 for job in "${JOBS[@]}"; do
