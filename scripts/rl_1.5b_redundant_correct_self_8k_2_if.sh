0ae2420fff434293d22d25f6619b30fe6c98144e
Lucky-259 <1436596125@qq.com>
2026-01-20 12:42:55 +0800

    update

--- scripts/rl_1.5b_redundant_correct_self_8k_2_if.sh
+++ scripts/rl_1.5b_redundant_correct_self_8k_2_if.sh
@@ -18,7 +18,7 @@ MODEL_PATH="/mnt/hdfs/if_au/models/DeepSeek-R1-Distill-Qwen-1.5B"
 PROJECT_NAME='Redundancy_new'
 EXPERIMENT_NAME='DS1.5B_8k_redundancy_correct_self_1_5e-4'
 RUN_DIR="$ROOT_DIR/checkpoints/${PROJECT_NAME}/${EXPERIMENT_NAME}"
-LOG_FILE="$ROOT_DIR/checkpoints/${PROJECT_NAME}_${EXPERIMENT_NAME}_new.log"
+LOG_FILE="/opt/tiger/hqz_debug/cky/verl-process/${PROJECT_NAME}_${EXPERIMENT_NAME}_new.log"
 mkdir -p "$RUN_DIR"
 export TENSORBOARD_DIR=$RUN_DIR
 
@@ -57,7 +57,7 @@ python3 -u -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.temperature=0.9 \
     actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.9 \
     actor_rollout_ref.rollout.val_kwargs.n=8 \
     trainer.critic_warmup=0 \
--- scripts/rl_1.5b_redundant_correct_self_8k_if.sh
+++ scripts/rl_1.5b_redundant_correct_self_8k_if.sh
@@ -16,16 +16,16 @@ ROOT_DIR=/mnt/hdfs/if_au/saves/cky
 MODEL_PATH="/mnt/hdfs/if_au/models/DeepSeek-R1-Distill-Qwen-1.5B"
 
 PROJECT_NAME='Redundancy_new'
-EXPERIMENT_NAME='DS1.5B_8k_redundancy_correct_self_1_1e-3'
+EXPERIMENT_NAME='DS1.5B_8k_redundancy_correct_self_2_1e-3'
 RUN_DIR="$ROOT_DIR/checkpoints/${PROJECT_NAME}/${EXPERIMENT_NAME}"
-LOG_FILE="$ROOT_DIR/checkpoints/${PROJECT_NAME}_${EXPERIMENT_NAME}_new.log"
+LOG_FILE="/opt/tiger/hqz_debug/cky/verl-process/${PROJECT_NAME}_${EXPERIMENT_NAME}_new.log"
 mkdir -p "$RUN_DIR"
 export TENSORBOARD_DIR=$RUN_DIR
 
 python3 -u -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     reward_model.reward_manager=redundancy \
-    +reward_model.reward_kwargs.alpha=1 \
+    +reward_model.reward_kwargs.alpha=2 \
     +reward_model.reward_kwargs.beta=1e-3 \
     +reward_model.reward_kwargs.extraction=self \
     +reward_model.reward_kwargs.way=correct \
@@ -57,7 +57,7 @@ python3 -u -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.temperature=0.9 \
     actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.9 \
     actor_rollout_ref.rollout.val_kwargs.n=8 \
     trainer.critic_warmup=0 \
