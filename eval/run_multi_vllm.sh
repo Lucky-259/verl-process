3e0e1dc0db0d704706ccf249bae0155816ac6418
Lucky-259 <1436596125@qq.com>
2026-03-31 02:57:07 +0800

    update

--- eval/run_multi_vllm.sh
+++ eval/run_multi_vllm.sh
@@ -14,15 +14,16 @@ BASE_SCRIPT="eval/vllm_all_8k.sh"   # 改成你的实际脚本路径
 
 # 你可以在这里列出多组：PROJECT_NAME EXPERIMENT_NAME [REPEAT] [CONCURRENCY]
 JOBS=(
+  "Redundancy_DAPO DS1.5B_8k_redundancy_self_correct_1_5e-4_DAPO 16 150 250"
   # "Redundancy_DAPO DS1.5B_8k_redundancy_self_correct_1_5e-4_1_DAPO 16 150"
   # "Redundancy_DAPO DS7B_8k_redundancy_self_correct_1_5e-4_1_DAPO 16 150"
   # "Redundancy_DAPO DS1.5B_8k_DAPO_base 16 150"
   # "Redundancy_DAPO DS1.5B_8k_1e_4_4096_DAPO_l1 16 150"
   # "Redundancy_DAPO DS1.5B_8k_2e_4_7168_DAPO_l1 16 150"
-  "Redundancy_DAPO qwen3_1.7B_8k_redundancy_self_correct_1_5e-4_DAPO 16 150 50,100,150,200"
-  "Redundancy_DAPO qwen3_1.7B_8k_DAPO_base 16 150 50,100,150,200"
+  # "Redundancy_DAPO qwen3_1.7B_8k_redundancy_self_correct_1_5e-4_DAPO 16 150 50,100,150,200"
+  # "Redundancy_DAPO qwen3_1.7B_8k_DAPO_base 16 150 50,100,150,200"
   # "Redundancy_DAPO DS7B_8k_DAPO_base 16 150 100,200,250,300"
-  "Redundancy_DAPO qwen3_8B_8k_redundancy_self_correct_1_5e-4_DAPO 16 150 50,100,150,200"
+  # "Redundancy_DAPO qwen3_8B_8k_redundancy_self_correct_1_5e-4_DAPO 16 150 50,100,150,200"
   # "Redundancy_DAPO DS1.5B_8k_redundancy_self_correct_1_5e-4_DAPO 16 150"
   # "Redundancy_DAPO DS7B_8k_redundancy_self_correct_1_2e-4_1_DAPO 16 150"
   # "Redundancy DS1.5B_8k_redundancy_correct_self_1_2e-4 16 150"
